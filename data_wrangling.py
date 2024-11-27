d98d75d3ffb5ae2bebcaf2d8e738f520915b62b7
bpsopra <bryan.parahy@soprasteria.com>
2024-11-27 10:20:48 +0100

    adding  data wrangling

---
+++ data_wrangling.py
@@ -0,0 +1,34 @@
+import calculator
+import pandas as pd
+import numpy as np
+import random
+
+
+# Load data
+
+df = pd.read_excel('default_of_credit_card_clients_V2.xls',header = 0)
+df = df.rename(columns={"default payment next month": "Y"})
+
+# Data processing
+df[['PAY_0','PAY_2']] = df[['PAY_0','PAY_2']].apply(lambda x: x/1000)
+
+# Data normalization
+
+numeric_cols = df.select_dtypes(include=np.number).columns.tolist()
+string_cols = ["ID","SEX","EDUCATION","MARRIAGE","Y"]
+
+for col in string_cols:
+    df[col] = df[col].astype(str)
+
+L_rand_nums = random.sample(range(1, 100), 2)
+constant = calculator.add(L_rand_nums[0],L_rand_nums[1])
+
+df_numeric = df[numeric_cols]
+df_numeric["PAY_0"] = df_numeric["PAY_0"] .apply(lambda x: x + constant)
+df_numeric_normalized = (df_numeric-df_numeric.min())/(df_numeric.max()-df_numeric.min())
+
+df_normalized_all = pd.concat([df[string_cols],df_numeric_normalized], axis= 1)
+
+# Get 5 first rows
+print(df_normalized_all.head())
+
